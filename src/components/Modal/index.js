841f52eb672b56ff518db67e73c21ea3ffd00180
Victor <cura.inthelab@gmail.com>
2023-10-17 23:28:55 +0900

    ADD Modal (component)

---
+++ src/components/Modal/index.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import Types from 'prop-types';
+import styles from './styles.moudle.scss';
+
+const Modal = ({ show, children, onClose, cancelable = true }) => {
+    useEffect(() => {
+        document.body.style.overflow = show ? 'overflow' : 'auto';
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [show]);
+
+    if (show) {
+        document.body.style.overflow = 'hidden';
+
+        return createPortal(
+            <div className={styles.ModalContainer}>
+                <button
+                    className={styles.CloseBackground}
+                    onClick={(e) => {
+                        if (cancelable) onClose();
+                        e.stopPropagation();
+                        e.preventDefault();
+                    }}
+                />
+                <button className={styles.ModalBackground}>{children}</button>
+            </div>,
+            document.querySelector('#modal-portal'),
+        );
+    }
+
+    return null;
+};
+
+Modal.propTypes = {
+    children: Types.node,
+    show: Types.bool.isRequired,
+    onClose: Types.func.isRequired,
+    cancelable: Types.bool,
+};
+
+export default Modal;
